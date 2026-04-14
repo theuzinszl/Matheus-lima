33c72728a09f9c7503946a3f59ceb337e2f86871
theuz <matheuslima030597ma@gmail.com>
2026-04-14 19:54:43 -0300

    Exercicio ArrayList

--- AtividadeOB/src/Ex10.java
+++ AtividadeOB/src/Ex10.java
@@ -1,7 +1,18 @@
 
 public class Ex10 {
 	
-	public static void main(String[] args) {
-		
+	    public static void main(String[] args) {
+
+	        OnibusEscolar onibus = new OnibusEscolar(50, 1);
+	        System.out.println("Alunos: " + onibus.getQuantidadeEstudantes());
+	        System.out.println("Professores: " + onibus.getQuantidadeProfessores());
+
+	        
+	        onibus.setQuantidadeProfessores(0);
+	        onibus.setQuantidadeEstudantes(30);
+
+	        System.out.println("\nApós remover professores:");
+	        System.out.println("Alunos: " + onibus.getQuantidadeEstudantes());
+	        System.out.println("Professores: " + onibus.getQuantidadeProfessores());
+	    }
 	}
-}
--- AtividadeOB/src/OnibusEscolar.java
+++ AtividadeOB/src/OnibusEscolar.java
@@ -1,4 +1,49 @@
-
 public class OnibusEscolar {
 
+    private int quantidadeEstudantes;
+    private int quantidadeProfessores;
+
+   
+    public OnibusEscolar(int quantidadeEstudantes, int quantidadeProfessores) {
+        setQuantidadeProfessores(quantidadeProfessores);
+        setQuantidadeEstudantes(quantidadeEstudantes);
+    }
+
+    
+    public int getQuantidadeEstudantes() {
+        return quantidadeEstudantes;
+    }
+
+    
+    public void setQuantidadeEstudantes(int quantidadeEstudantes) {
+
+       
+        if (this.quantidadeProfessores == 0) {
+            this.quantidadeEstudantes = 0;
+            return;
+        }
+
+      
+        if (quantidadeEstudantes > 40) {
+            System.out.println("Máximo de alunos excedido. Ajustado para 40.");
+            this.quantidadeEstudantes = 40;
+        } else {
+            this.quantidadeEstudantes = quantidadeEstudantes;
+        }
+    }
+
+    
+    public int getQuantidadeProfessores() {
+        return quantidadeProfessores;
+    }
+
+    // Setter professores
+    public void setQuantidadeProfessores(int quantidadeProfessores) {
+        this.quantidadeProfessores = quantidadeProfessores;
+
+        // Se não houver professores, zera alunos automaticamente
+        if (quantidadeProfessores == 0) {
+            this.quantidadeEstudantes = 0;
+        }
+    }
 }
